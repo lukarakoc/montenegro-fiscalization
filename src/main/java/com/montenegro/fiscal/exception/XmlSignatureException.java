a69df73e8cc6174068e4098116618dfb4ada8093
Luka Rakocevic <rakocevic.luka@gmail.com>
2025-05-13 12:44:24 +0200

    Removed unnecessary constructor

--- src/main/java/com/montenegro/fiscal/exception/XmlSignatureException.java
+++ src/main/java/com/montenegro/fiscal/exception/XmlSignatureException.java
@@ -2,10 +2,6 @@ package com.montenegro.fiscal.exception;
 
 public class XmlSignatureException extends FiscalizationException {
 
-  public XmlSignatureException(String msg) {
-    super(msg);
-  }
-
   public XmlSignatureException(String msg, Throwable cause) {
     super(msg, cause);
   }
